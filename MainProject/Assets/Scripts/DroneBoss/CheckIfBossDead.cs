a7e4fdc569fcfa51429b452bc92ae0a8cc9235c8
Murphyy1996 <31962997+Murphyy1996@users.noreply.github.com>
2018-03-25 17:08:23 +0100

    New foundry now has an end condition
    Kill daddy drone

---
+++ MainProject/Assets/Scripts/DroneBoss/CheckIfBossDead.cs
@@ -0,0 +1,23 @@
+﻿//Author: James Murphy
+//Purpose: Check if the drone boss is dead
+//Requirements: A drone to check
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckIfBossDead : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject droneBoss;
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (droneBoss == null)
+        {
+            SceneManager.LoadScene("SpeedRun");
+        }
+    }
+}
