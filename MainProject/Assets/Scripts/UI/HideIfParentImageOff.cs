ebbc2fe24223556ab78d9dbf88d66672a8107a07
James Murphy <murphyy@hotmail.co.uk>
2018-04-05 13:27:47 +0100

    Keyboard buttons show for what guns you pick up

---
+++ MainProject/Assets/Scripts/UI/HideIfParentImageOff.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideIfParentImageOff : MonoBehaviour
+{
+    [SerializeField]
+    private Image thisImage, parentImage; 
+
+    private void Start()
+    {
+        thisImage = GetComponent<Image>();
+        parentImage = transform.parent.GetComponent<Image>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (parentImage != null)
+        {
+            if (parentImage.enabled == true)
+            {
+                thisImage.enabled = true;
+            }
+            else
+            {
+                thisImage.enabled = false;
+            }
+        }
+    }
+}
