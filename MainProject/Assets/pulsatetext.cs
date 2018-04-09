ee753e87a24daad3a97693a88fc695920842d214
KG182501 <37618786+KG182501@users.noreply.github.com>
2018-04-09 15:37:55 +0100

    out of ammo text now pulses a lil

---
+++ MainProject/Assets/pulsatetext.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pulsatetext : MonoBehaviour {
+
+    [SerializeField]
+    private Outline oLine;
+    [SerializeField]
+    private Text ah;
+    [SerializeField]
+    private float speedOfFlash;
+
+	// Use this for initialization
+	void Start ()
+    {
+        oLine = this.gameObject.GetComponent<Outline>();
+        oLine.enabled = false;
+        ah = this.gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (ah.enabled == true)
+        {
+            //run method for flashing
+            StartCoroutine("flash");
+        }
+        else
+        {
+            return;
+        }
+	}
+
+    IEnumerator flash()
+    {
+        oLine.enabled = true;
+        yield return new WaitForSeconds(speedOfFlash);
+        oLine.enabled = false;
+    }
+
+
+}
