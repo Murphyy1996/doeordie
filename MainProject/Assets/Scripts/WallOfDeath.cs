71f4d15ab843c3459b1cc72faf52b1e4e19e9b2c
James Cook <fizzy726@hotmail.com>
2018-04-19 10:21:40 +0100

    Wall of Death
    JLADJFILAJ

---
+++ MainProject/Assets/Scripts/WallOfDeath.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallOfDeath : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 1f;
+
+    [SerializeField]
+    GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+		
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+}
