96342a345864518210ec83d97515469df24cb196
Murphyy1996 <31962997+Murphyy1996@users.noreply.github.com>
2018-04-23 18:50:53 +0100

    Mouse will now be forced to centre in gameplay but not on menus

---
+++ MainProject/Assets/Scripts/ForceMouseIntoPosition.cs
@@ -0,0 +1,29 @@
+﻿//Author: James Murphy
+//Purpose: Will force the mouse into the correct position
+//Requirements: Goes on the player prefab
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceMouseIntoPosition : MonoBehaviour
+{
+    private void Awake() //Control the mouse on the tick, independent of timescale
+    {
+        Invoke("Tick", 0.1f);
+    }
+
+    private void Tick()
+    {
+        if (Time.timeScale != 0)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+    }
+}
