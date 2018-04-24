0eacd7aa6796a42678e474cd0cbeead4ccc14098
Sic5 <37619002+Sic5@users.noreply.github.com>
2018-04-24 15:15:35 +0100

    Added shooting sound to ranged prefab

---
+++ MainProject/Assets/Scripts/AIRelated/AITrees/PlayAudioSource.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class PlayAudioSource : Action
+{
+
+    public AudioSource audioSource;
+
+    public override TaskStatus OnUpdate()
+    {
+        audioSource.enabled = true;
+
+        StartCoroutine("TurnOffClip");
+
+        return TaskStatus.Success; 
+    }
+
+    IEnumerator TurnOffClip()
+    {
+        yield return new WaitForSeconds(0.5f);
+        audioSource.enabled = false;
+        yield return null;
+    }
+
+
+}
